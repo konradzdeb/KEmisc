db2ef5de1fdb9cb2199c4f34d42c79a57eeeee30
Konrad Zdeb <konrad.zdeb@me.com>
2018-06-07 07:59:40 +0100

    Added readme and other package dev stuff

--- R/read_RDS_to_global.R
+++ R/read_RDS_to_global.R
@@ -1,3 +1,22 @@
+#' @title Read RDS Ffile Directly to Global Environment
+#'
+#' @description
+#'
+#' @param file_path
+#' @param verbose A logica, if \code{TRUE}
+#'
+#' @return A R object corresponding to \code{basename} on \code{file_path}
+#'   argument.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   tmpCars <- tempfile(pattern = "mtcars_", fileext = ".RDS")
+#'   saveRDS(object = mtcars, file = tmpCars)
+#'   read_RDS_to_global(tmpCars)
+#' }
+#'
 read_RDS_to_global <- function(file_path, verbose = TRUE) {
     readRDS(file = file_path) -> dta
     base_name <- basename(tools::file_path_sans_ext(file_path))
@@ -6,11 +25,9 @@ read_RDS_to_global <- function(file_path, verbose = TRUE) {
            envir = globalenv())
     if (verbose) {
         if (checkmate::allMissing(setdiff(base_name, ls(envir = globalenv())))) {
-            cat(crayon::green(
-                "Created object:",
+            cat("Created object:",
                 base_name,
-                "in global environment."
-            ))
+                "in global environment.")
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,12 @@
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+Kmisc
+=====
+
+The goal of Kmisc is to …
+
+Installation
+------------
+
+``` r
+## basic example code
+```
