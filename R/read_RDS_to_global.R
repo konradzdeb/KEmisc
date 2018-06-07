e8205fcea0f45e30eecc5b1db64cd21bb39e33ae
Konrad Zdeb <konrad.zdeb@me.com>
2018-06-07 17:15:33 +0100

    Check file path and upgrade msg generation mechanism.

--- R/read_RDS_to_global.R
+++ R/read_RDS_to_global.R
@@ -16,6 +16,7 @@
 #'   argument.
 #'
 #' @importFrom checkmate allMissing
+#' @importFrom checkmate assert_access
 #'
 #' @export
 #'
@@ -27,16 +28,24 @@
 #' }
 #'
 read_RDS_to_global <- function(file_path, verbose = TRUE) {
+
+    # Check if path is accessible
+    checkmate::assert_access(x = file_path, access = "r")
+
+
     readRDS(file = file_path) -> dta
     base_name <- basename(tools::file_path_sans_ext(file_path))
     assign(x = base_name,
            value = dta,
            envir = globalenv())
     if (verbose) {
+        msg <- paste("Created object:",
+                     base_name,
+                     "in global environment.", collapse = " ")
         if (checkmate::allMissing(setdiff(base_name, ls(envir = globalenv())))) {
-            cat("Created object:",
-                base_name,
-                "in global environment.")
+            cat(msg)
+        } else {
+            cat(msg)
         }
     }
 }
