3bd57da7de815e71e448f1ff748c4afcb35f0e2d
Konrad Zdeb <konrad.zdeb@me.com>
2018-06-12 07:52:33 +0100

    Produce scaled unit without zeros.

---
+++ R/units_without_zeros.R
@@ -0,0 +1,31 @@
+#' @title Produce Formatted Unit Without Zero
+#'
+#' @param x A numeric vector
+#' @param ... other arguments passed to \code{\link[scales]{unit_format}}
+#'
+#' @return A character vector.
+#'
+#' @export
+#'
+#' @examples
+#' vec_tst <- c(0, 1000, 1e6)
+#' units_without_zero(vec_tst, unit = "K", scale = 1e-3, sep = "")
+#' # "0"      "1K"     "1,000K"
+#' # For comparison
+#' scales::unit_format(unit = "K", scale = 1e-3, sep = "")(vec_tst)
+#' # [1] "0K"     "1K"     "1,000K"
+#'
+units_without_zero <- function(x, ...) {
+    sf <- scales::unit_format(...)
+    f <- function(lbl) {
+        if (is.na(lbl)) {
+            NA
+        } else if (lbl == 0) {
+            "0"
+        } else {
+            sf(x = lbl)
+        }
+    }
+    vf <- Vectorize(FUN = f, vectorize.args = c("lbl"))
+    return(vf(x))
+}
