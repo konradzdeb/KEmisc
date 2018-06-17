e89fe77e6214faf50f265e895d25463adca0729d
Konrad Zdeb <konrad.zdeb@me.com>
2018-06-17 23:14:38 +0100

    Added test for getting matrix triangle

---
+++ tests/testthat/test_get_triangle.R
@@ -0,0 +1,7 @@
+context("Test getting matrix upper or lower part")
+
+test_that(desc = "Returned object is a matrix",
+          code = expect_is(
+              object = get_triangle(cormat = matrix(1:20, 5, 5), part = "upper"),
+              class = "matrix"
+          ))
