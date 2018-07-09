53362c17932ffb9952faf7fe475b42a702123c34
Konrad Zdeb <konrad.zdeb@me.com>
2018-07-09 08:10:37 +0100

    More sensible R version, started function to look for frequencies.

---
+++ R/preview_frequency_table.R
@@ -0,0 +1,16 @@
+#' @title Preview Frequency Tables
+#'
+#' @description Simplefunction showing frequencies of vector by combining
+#'   results from \code{\link[base]{table}} and \code{\link[base]{prop.table}}.
+#'
+#' @param x A vector.
+#'
+#' @return A data frame with frequency values of a vector
+#' @export
+#'
+#' @examples
+#' preview_frequency_table(x = c("a", rep("b", 3)))
+#'
+preview_frequency_table <- function(x) {
+    # Check if passed value is a vector
+}
