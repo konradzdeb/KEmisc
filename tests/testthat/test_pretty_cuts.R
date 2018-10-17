09e8da8943509323f4e570ebe174411784bec542
Konrad Zdeb <konrad.zdeb@me.com>
2018-10-17 17:17:40 +0100

    Hmisc package existence check is no longer needed.

--- tests/testthat/test_pretty_cuts.R
+++ tests/testthat/test_pretty_cuts.R
@@ -2,32 +2,28 @@ context("Test formatting cut2 output")
 
 
 # Only run this test if Hmisc exists
-if (requireNamespace("Hmisc", quietly = TRUE)) {
-    test_that(desc = "Provided string correctly changes set notation",
-              code = expect_identical(
-                  object = pretty_cuts(
-                      cut_str = structure(
-                          1:5,
-                          .Label = c(
-                              "[   1,1302)",
-                              "[1302,2603)",
-                              "[2603,3904)",
-                              "[3904,5205)",
-                              "[5205,6505]"
-                          ),
-                          class = "factor"
+test_that(desc = "Provided string correctly changes set notation",
+          code = expect_identical(
+              object = pretty_cuts(
+                  cut_str = structure(
+                      1:5,
+                      .Label = c(
+                          "[   1,1302)",
+                          "[1302,2603)",
+                          "[2603,3904)",
+                          "[3904,5205)",
+                          "[5205,6505]"
                       ),
-                      only_cuts = TRUE
+                      class = "factor"
                   ),
-                  expected = c(
-                      "1 - 1301",
-                      "1302 - 2602",
-                      "2603 - 3903",
-                      "3904 - 5204",
-                      "5205 - 6505"
-                  )
-              ))
-}
+                  only_cuts = TRUE
+              ),
+              expected = c("1 - 1301",
+                           "1302 - 2602",
+                           "2603 - 3903",
+                           "3904 - 5204",
+                           "5205 - 6505")
+          ))
 
 test_that(
     desc = "Correct error message is produced on wrong string",
