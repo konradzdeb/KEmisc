0997ed6ca0d9e1207d59f5fa29d5ed4c82a87d6a
Konrad Zdeb <konrad.zdeb@me.com>
2018-10-20 17:34:22 +0100

    Added correct entries in NAMESPACE and expanded tests.

--- tests/testthat/test-defactorize.R
+++ tests/testthat/test-defactorize.R
@@ -1,7 +1,13 @@
 context("Test DeFactorization")
 
-test_that(desc = "Functiona fails on non-factors",
-          code = expect_error(
-              object = defactorize(x = 1),
-              regexp = "Error\\sin\\sassert_class.*"
-          ))
+test_that(
+    desc = "Functiona fails on non-factors",
+    code = expect_error(object = defactorize(x = 1),
+                        regexp = "Assertion on 'x' failed: Must have class 'factor', but has class*")
+)
+
+test_that(
+    desc = "Functiona fails on NULL factor",
+    code = expect_error(object = defactorize(as.factor(NULL)),
+                        regexp = "Assertion on 'x' failed: Contains only missing values.")
+)
