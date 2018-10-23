079ddfb75c316283ae8a6c2372d63c18c4dd28a9
Konrad Zdeb <konrad.zdeb@me.com>
2018-10-23 17:59:11 +0100

    Cleaning tests

--- tests/testthat/test-get_vector_type.R
+++
@@ -1,4 +0,0 @@
-context("Guessing factor content")
-
-test_that(desc = "Factor with letters only has mostly characters.",
-          code = expect_identical(object = guess_vector_type(x = as.factor(letters))))
