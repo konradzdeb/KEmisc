9f0512d621e8aeca3308ae91cb4af6307ab86e22
Konrad Zdeb <konrad.zdeb@me.com>
2018-10-24 20:48:59 +0100

    Better tests

--- tests/testthat/test_guess_vector_type.R
+++ tests/testthat/test_guess_vector_type.R
@@ -17,3 +17,31 @@ test_that(desc = "Integer is returned for factor with integers",
               runif(min = -1e4, max = 1e4, n = 1e3), 0
           ))),
           expected = "integer"))
+
+
+test_that(desc = "Correct class is guessedfor a mixed vector (mostly doubles)",
+          code = expect_identical(object = guess_vector_type(as.factor(
+              sapply(runif(n = 1e4), function(vec_elm) {
+                  if (vec_elm >= 0.8) {
+                      sample(x = letters, size = 1)
+                  } else {
+                      vec_elm
+                  }
+              })
+          )),
+          expected = "double"))
+
+
+
+test_that(desc = "Correct class is guessedfor a mixed vector (mostly integers)",
+          code = expect_identical(object = guess_vector_type(as.factor(
+              sapply(runif(n = 1e4), function(vec_elm) {
+                  if (vec_elm >= 0.2) {
+                      sample(x = 1:1e3, size = 1)
+                  } else {
+                      vec_elm
+                  }
+              })
+          )),
+          expected = "integer"))
+
