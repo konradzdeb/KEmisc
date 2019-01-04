9c565077fae090db3b85edf2eb3eea7605d960c5
Konrad Zdeb <konrad.zdeb@me.com>
2019-01-04 16:49:11 +0000

    Date file update
    Function generating dated file name now accepts extension arguments. Tests are also more comprehensive and check whether the produced file has the right extension and other desired string elements.
    
    Signed-off-by: Konrad Zdeb <konrad.zdeb@me.com>

--- R/create_dated_name.R
+++ R/create_dated_name.R
@@ -1,29 +1,43 @@
 #' @title Create Dated File/Path Name
 #'
-#' @description This is trival function that wraps \code{\link[base]{paste}}
-#'   call and prduces a string with \code{\link[base]{Sys.time}} results.
+#' @description This is trivial function wraps \code{\link[base]{paste}}
+#'   call and produces a string with \code{\link[base]{Sys.time}} and an
+#'   optional extension.
+#'
+#' @details This simple function is convenient for automatically generating
+#'   file names with the current date and/or time.
 #'
 #' @param x A character vector.
-#' @param timestamp_format A string scalar.
-#' @param sep A string scalar.
+#' @param timestamp_format A string scalar for time format, as in
+#'   \code{\link[base]{strptime}}.
+#' @param sep A string scalar to separate file name parts.
+#' @param extension A string scalar with file type extension.
 #'
 #' @return A character vector of length corresponding to x.
 #'
 #' @export
 #'
+#' @importFrom checkmate assertScalar
+#' @importFrom checkmate assert_character
+#'
 #' @examples
 #' create_dated_file_name(x = "file_name")
+#' create_dated_file_name(x = "annual_file", timestamp_format = "%Y")
+#' create_dated_file_name(x = "time_file", timestamp_format = "%H:%M:%S")
 create_dated_file_name <- function(x,
                                    timestamp_format = "%d-%m-%Y",
-                                   sep = "-") {
+                                   sep = "-",
+                                   extension) {
     # Arguments check
-    checkmate::checkScalar(x = timestamp_format,
-                           na.ok = FALSE,
-                           null.ok = FALSE)
-    checkmate::checkScalar(x = sep,
-                           na.ok = TRUE,
-                           null.ok = TRUE)
-    checkmate::check_character(
+    mapply(
+        FUN = assertScalar,
+        x = list(timestamp_format, sep),
+        MoreArgs = list(na.ok = FALSE,
+                        null.ok = FALSE),
+        SIMPLIFY = FALSE
+    )
+
+    assert_character(
         x = x,
         min.chars = 1,
         min.len = 1,
@@ -33,6 +47,18 @@ create_dated_file_name <- function(x,
     # Generate file name
     paste(x,
           format(Sys.time(), timestamp_format),
-          sep = sep)
+          sep = sep) -> file_name
+
+    # Conditionally add extension
+    if (!missing(extension)) {
+        # Check validity of provided extension argument
+        assertScalar(x = extension,
+                     na.ok = FALSE,
+                     null.ok = FALSE)
+        # Add to file name
+        paste0(file_name, extension) -> file_name
+    }
+
+    return(file_name)
 
 }
--- tests/testthat/test_dated_file_name.R
+++ tests/testthat/test_dated_file_name.R
@@ -1,11 +1,32 @@
-context("Dated file name creation")
+context("Dated file name")
 
 test_that(
-    desc = "Created file name is a string",
-    code = checkmate::expect_string(
-        x = create_dated_file_name(x = "file_name"),
-        na.ok = FALSE,
-        min.chars = 9,
-        null.ok = FALSE
+    desc = "File name has name string",
+    code = expect_match(
+        object = create_dated_file_name(x = "some_name"),
+        regexp = "^some_name.*",
+        perl = TRUE
+    )
+)
+
+test_that(
+    desc = "File name has extension",
+    code = expect_match(
+        object = create_dated_file_name(x = "some_name", extension = ".RDS"),
+        regexp = ".*RDS$",
+        perl = TRUE
+    )
+)
+
+test_that(
+    desc = "File name contains date digits",
+    code = expect_match(
+        object = create_dated_file_name(
+            x = "some_name",
+            extension = ".RDS",
+            timestamp_format = "%Y"
+        ),
+        regexp = ".*\\d{4}\\.RDS$",
+        perl = TRUE
     )
 )
