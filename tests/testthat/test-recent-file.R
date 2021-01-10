663cc5be5b7f3c9d7870062ed6797b272d12ef4f
Konrad Zdeb <konrad.zdeb@me.com>
2021-01-10 13:48:15 +0000

    File search tests
    Ensuring that file_two is always most recent

--- tests/testthat/test-recent-file.R
+++ tests/testthat/test-recent-file.R
@@ -1,6 +1,7 @@
 test_that("Recent file is found", {
     withr::local_tempdir(pattern = "test_fixtures/file_one")
-    withr::local_tempdir(pattern = "test_fixtures/file_two")
+    path_file_two <- withr::local_tempdir(pattern = "test_fixtures/file_two")
+    Sys.setFileTime(path_file_two, Sys.time() + 1e6)
     search_res <- most_recent(fs::path_temp("test_fixtures"))
     expect_length(search_res, 1)
     expect_true(grepl("file_two", search_res))
