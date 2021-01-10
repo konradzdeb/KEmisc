d9d52fb8528797b60c5aacc3f3ce2f9b4bce71ed
Konrad Zdeb <konrad.zdeb@me.com>
2021-01-10 12:49:49 +0000

    Fixed tests

--- tests/testthat/test-recent-file.R
+++ tests/testthat/test-recent-file.R
@@ -1,7 +1,7 @@
 test_that("Recent file is found", {
-    withr::local_tempdir(pattern = "file_one")
-    withr::local_tempdir(pattern = "file_two")
-    search_res <- most_recent(tempdir())
+    withr::local_tempdir(pattern = "test_fixtures/file_one")
+    withr::local_tempdir(pattern = "test_fixtures/file_two")
+    search_res <- most_recent(fs::path_temp("test_fixtures"))
     expect_length(search_res, 1)
     expect_true(grepl("file_two", search_res))
 })
