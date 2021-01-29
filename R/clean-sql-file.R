8d758e344065b72248edb6f329d2cff8dbd4d3cb
Konrad Zdeb <konrad.zdeb@me.com>
2021-01-29 17:02:11 +0000

    Clean SQL Stub
    Function stub for cleaning SQL files

---
+++ R/clean-sql-file.R
@@ -0,0 +1,13 @@
+#' Clean SQL File
+#'
+#' Cleans SQL file removing comments and collapsing all the lines together.
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+clean_sql_file <- function(x) {
+
+}
