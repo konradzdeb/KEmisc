b7c303a35b9e0f9f574b2b90cd29cde3f267593c
Konrad Zdeb <konrad.zdeb@me.com>
2021-02-17 14:02:09 +0000

    Function stub

--- R/view-glimpse.R
+++ R/view-glimpse.R
@@ -1,6 +1,7 @@
-#' @title Show d
+#' @title Provide glimpse-like results in View
 #'
 #' @param x A data frame or a tibble
+#' @param n A number of observations to show
 #'
 #' @return NULL
 #'
@@ -11,6 +12,13 @@
 #' \dontrun{
 #' view_glimpse(mtcars)
 #' }
-view_glimpse <- function(x) {
-    res_dta <- data.frame(column = names(x))
+view_glimpse <- function(x, n = 5) {
+    x_cols_names <- colnames(x)
+    x_t <- t(head(x, n = n))
+    col_nms <- rownames(x_t)
+    res_df <- cbind(col_nms, x_t)
+    res_df <- setNames(object = as.data.frame(res_df),
+             nm = c("column_name", paste0("obs_no_", 2:ncol(res_df))))
+    rownames(res_df) <- NULL
+    res_df
 }
